234100cb8df49bb6b1b0940e7bda5ccb786d28d0
Jacek Kotlarski <szumak@virthost.pl>
2025-01-23 21:07:41 +0100

    `Refactored BDbHandler class in base.py to use _get_data and _set_data methods.`

--- jskworkclock/libs/base.py
+++ jskworkclock/libs/base.py
@@ -6,10 +6,8 @@
   Purpose: Sets of base classes.
 """
 
-from inspect import currentframe
 
 from jsktoolbox.basetool.data import BData
-from jsktoolbox.raisetool import Raise
 
 from libs.keys import Keys
 from libs.database import Database
@@ -21,20 +19,12 @@ class BDbHandler(BData):
     @property
     def _db_handler(self) -> Database:
         """The _db_handler property."""
-        if Keys.DBH not in self._data:
-            self._data[Keys.DBH] = None
-        return self._data[Keys.DBH]
+        return self._get_data(key=Keys.DBH)  # type: ignore
 
     @_db_handler.setter
     def _db_handler(self, value: Database) -> None:
-        if not isinstance(value, Database):
-            raise Raise.error(
-                f"Expected Database type, received '{type(value)}'.",
-                TypeError,
-                self._c_name,
-                currentframe(),
-            )
-        self._data[Keys.DBH] = value
+        """The _db_handler setter."""
+        self._set_data(key=Keys.DBH, value=value, set_default_type=Database)
 
 
 # #[EOF]#######################################################################
